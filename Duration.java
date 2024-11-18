b54f23c46fd00511089a5ff4cd4c92e332c02931
MaxGof68 <101732816+MaxGof68@users.noreply.github.com>
2024-11-18 13:03:05 +0000

    getsetdur

---
+++ Duration.java
@@ -0,0 +1,20 @@
+Public Class duration {
+    int hours;
+    int minutes;
+    int seconds;
+
+Public duration(int hours, int minutes, int seconds) {
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
+}
+Public getSeconds() {
+    return seconds;
+}
+Public getMinutes() {
+    return minutes;
+}
+Public getHours() {
+    return hours;
+}
+} 
